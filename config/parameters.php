723c80a1d0d99e7ef70c0cfa92fa33aa95c79e63
Nouu <noelarrieulacoste@yahoo.fr>
2021-04-03 12:20:00 +0200

    add default TZ to avoid crash

--- config/parameters.php
+++ config/parameters.php
@@ -1,4 +1,5 @@
 <?php
+// Database parameters
 $paramDbType = getenv('PJT_DB_TYPE');
 $paramDbHost = getenv('PJT_DB_HOST');
 $paramDbPort = getenv('PJT_DB_PORT');
@@ -6,6 +7,8 @@ $paramDbUser = getenv('PJT_DB_USER');
 $paramDbPassword = getenv('PJT_DB_PASSWORD');
 $paramDbName = getenv('PJT_DB_NAME');
 $paramDbPrefix = getenv('PJT_DB_PREFIX');
+
+// Other parameters
 $SslKey = getenv('PJT_SSL_KEY');
 $SslCert = getenv('PJT_SSL_CERT');
 $SslCa = getenv('PJT_SSL_CA');
@@ -15,3 +18,4 @@ $enforceUTF8 = getenv('PJT_ENFORCE_UTF8');
 $documentRoot = '/mnt/documents/';
 $logFile = '/mnt/logs/projeqtor_${date}.log';
 $paramDefaultLocale = 'en';
+$paramDefaultTimezone="Europe/Paris";
